01991f6f648ac303fc953bcd73d5b861ecf88df1
Uncle88 <Uncle880928>
2016-07-22 10:47:32 +0300

    Created the class PalindromClass

---
+++ PalindromTestApp/PalindromTestApp/PalindromClass.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PalindromTestApp
+{
+   partial class PalindromClass
+    {
+        public static bool Palindromtest(string str)
+        {
+            for (int i = 1, j = str.Length - 1; i < j; i++, j--)
+
+                if (str[i] != str[j])
+                    return false;
+            return true;
+
+        }
+    }
+}
