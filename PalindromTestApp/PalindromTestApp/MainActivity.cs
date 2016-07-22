d2f172586c4d184bd27c585dcd51795c0063a1fe
Uncle88 <Uncle880928>
2016-07-22 23:13:30 +0300

    App without Seekbar(use backgraundColor)

--- PalindromTestApp/PalindromTestApp/MainActivity.cs
+++ PalindromTestApp/PalindromTestApp/MainActivity.cs
@@ -24,77 +24,6 @@ namespace PalindromTestApp
 
             Button button = FindViewById<Button>(Resource.Id.MyButton);
             button.Click += OnButtonClicked;
-
-           SeekBar seekbar = FindViewById<SeekBar>(Resource.Id.seekBar1);
-           seekbar.StopTrackingTouch += BlurImageHandler;
-        }
-
-        private void BlurImageHandler(object sender, SeekBar.StopTrackingTouchEventArgs e)
-        {
-            int radius = e.SeekBar.Progress;
-            if (radius == 0)
-            {
-                ImageView imageView = FindViewById<ImageView>(Resource.Id.container);
-                // We don't want to blur, so just load the un-altered image.
-                imageView.SetImageResource(Resource.Drawable.winter);
-            }
-            else
-            {
-                DisplayBlurredImage(radius);
-            }
-        }
-        private void DisplayBlurredImage(int radius)
-        {
-            SeekBar seekbar = FindViewById<SeekBar>(Resource.Id.seekBar1);
-            ImageView imageView = FindViewById<ImageView>(Resource.Id.container);
-            seekbar.StopTrackingTouch -= BlurImageHandler;
-            seekbar.Enabled = false;
-
-           // ShowIndeterminateProgressDialog();
-
-            Task.Factory.StartNew(() => {
-                Bitmap bmp = CreateBlurredImage(radius);
-                return bmp;
-            })
-            .ContinueWith(task => {
-                Bitmap bmp = task.Result;
-                imageView.SetImageBitmap(bmp);
-                seekbar.StopTrackingTouch += BlurImageHandler;
-                seekbar.Enabled = true;
-               // DismissIndeterminateProgressDialog();
-            }, TaskScheduler.FromCurrentSynchronizationContext());
-        }
-
-        private Bitmap CreateBlurredImage(int radius)
-        {
-            // Load a clean bitmap and work from that.
-            Bitmap originalBitmap = BitmapFactory.DecodeResource(Resources, Resource.Drawable.winter);
-
-            // Create another bitmap that will hold the results of the filter.
-            Bitmap blurredBitmap;
-            blurredBitmap = Bitmap.CreateBitmap(originalBitmap);
-
-            // Create the Renderscript instance that will do the work.
-            RenderScript rs = RenderScript.Create(this);
-
-            // Allocate memory for Renderscript to work with
-            Allocation input = Allocation.CreateFromBitmap(rs, originalBitmap, Allocation.MipmapControl.MipmapFull, AllocationUsage.Script);
-            Allocation output = Allocation.CreateTyped(rs, input.Type);
-
-            // Load up an instance of the specific script that we want to use.
-            ScriptIntrinsicBlur script = ScriptIntrinsicBlur.Create(rs, Element.U8_4(rs));
-            script.SetInput(input);
-
-            // Set the blur radius
-            script.SetRadius(radius);
-
-            // Start the ScriptIntrinisicBlur
-            script.ForEach(output);
-
-            // Copy the output to the blurred bitmap
-            output.CopyTo(blurredBitmap);
-
-            return blurredBitmap;
         }
 
         private void OnButtonClicked(object sender, EventArgs e)
@@ -105,6 +34,7 @@ namespace PalindromTestApp
             string toast = string.Format(" {0} ", PalindromClass.Palindromtest(a));
             Toast.MakeText(this, toast, ToastLength.Long).Show();
         }
+
         protected override void OnStop()
         {
             Button button = FindViewById<Button>(Resource.Id.MyButton);//dich'
--- PalindromTestApp/PalindromTestApp/Resources/Resource.Designer.cs
+++ PalindromTestApp/PalindromTestApp/Resources/Resource.Designer.cs
@@ -63,26 +63,23 @@ namespace PalindromTestApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050006
-			public const int MyButton = 2131034118;
+			// aapt resource value: 0x7f050005
+			public const int MyButton = 2131034117;
 			
 			// aapt resource value: 0x7f050001
 			public const int container = 2131034113;
 			
-			// aapt resource value: 0x7f050005
-			public const int edittext = 2131034117;
+			// aapt resource value: 0x7f050004
+			public const int edittext = 2131034116;
 			
 			// aapt resource value: 0x7f050000
 			public const int scroll = 2131034112;
 			
 			// aapt resource value: 0x7f050002
-			public const int seekBar1 = 2131034114;
+			public const int textView1 = 2131034114;
 			
 			// aapt resource value: 0x7f050003
-			public const int textView1 = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int textView2 = 2131034116;
+			public const int textView2 = 2131034115;
 			
 			static Id()
 			{
