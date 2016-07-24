b65dc3cea15c55048af6869fd32de5d8b48e5d8c
Uncle88 <Uncle880928>
2016-07-24 20:22:07 +0300

    resource use rounded corners

--- PalindromTestApp/PalindromTestApp/Resources/Resource.Designer.cs
+++ PalindromTestApp/PalindromTestApp/Resources/Resource.Designer.cs
@@ -45,13 +45,19 @@ namespace PalindromTestApp
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int button_rounded = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int imag = 2130837505;
+			public const int Icon = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int winter = 2130837506;
+			public const int imag = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int rounded = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int winter = 2130837508;
 			
 			static Drawable()
 			{
